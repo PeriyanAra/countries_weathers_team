644f1c47b96dc5f29726ce964de5657908b5cf8c
Ara Periyan <periyan.ara@gmail.com>
2023-02-18 15:02:50 +0400

    change abstract class

--- lib/domain/countries/repasitory/country_repository.dart
+++ lib/domain/countries/repasitory/country_repository.dart
@@ -1,8 +1,6 @@
 import 'package:countries_and_weathers/domain/countries/entity/export.dart';
 import 'package:countries_and_weathers/domain/result/export.dart';
 
-class CountryRepository {
-  Future<Result<CountryEntity, FailureResult>> getCountryByName(String countryName) {
-    return getCountryByName(countryName);
-  }
+abstract class CountryRepository {
+  Future<Result<CountryEntity, FailureResult>> getCountryByName(String countryName);
 }
