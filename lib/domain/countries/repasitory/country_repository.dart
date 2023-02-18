4bb682d4bc041167e82bd926bb7b26ab3c39ee80
Ara Periyan <periyan.ara@gmail.com>
2023-02-18 15:00:45 +0400

    rename repository

--- lib/domain/countries/repasitory/country_repository.dart
+++ lib/domain/countries/repasitory/country_repository.dart
@@ -1,7 +1,7 @@
 import 'package:countries_and_weathers/domain/countries/entity/export.dart';
 import 'package:countries_and_weathers/domain/result/export.dart';
 
-class XPageRepository {
+class CountryRepository {
   Future<Result<CountryEntity, FailureResult>> getCountryByName(String countryName) {
     return getCountryByName(countryName);
   }
