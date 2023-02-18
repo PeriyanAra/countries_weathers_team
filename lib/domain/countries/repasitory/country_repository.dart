2f20a5ea9a0a87feda186ca9b5be77db50e31f99
Ara Periyan <periyan.ara@gmail.com>
2023-02-18 15:03:04 +0400

    formatter fix

--- lib/domain/countries/repasitory/country_repository.dart
+++ lib/domain/countries/repasitory/country_repository.dart
@@ -2,5 +2,7 @@ import 'package:countries_and_weathers/domain/countries/entity/export.dart';
 import 'package:countries_and_weathers/domain/result/export.dart';
 
 abstract class CountryRepository {
-  Future<Result<CountryEntity, FailureResult>> getCountryByName(String countryName);
+  Future<Result<CountryEntity, FailureResult>> getCountryByName(
+    String countryName,
+  );
 }
